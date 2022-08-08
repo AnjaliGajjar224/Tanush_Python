0bde2552b07ca16a12f376bf6f3f9c1261dac1cb
AnjaliGajjar224 <98864149+AnjaliGajjar224@users.noreply.github.com>
2022-08-08 21:31:06 +0530

    Add files via upload

---
+++ 8thAugust.py
@@ -0,0 +1,65 @@
+# print("Tanush Said:'Good Evening'")
+# print('Prachi Said:"How are you?"')
+
+# print("Hello World",end=" ")
+# print("Hey")
+
+# print("Hello World",end="\t")
+# print("Hey")
+
+
+# print("Hello World",end="Tanush")
+# print("Hey")
+
+
+# print("Hello World",end="\n")
+# print("Hey")
+
+# print("Hello world",end="")
+# print("Hey")
+
+"""
+Escaping Sequence
+
+\n ---> new line
+\t ---> tab
+\b ---> backspace
+\r ---> carriage return
+\
+"""
+
+# print("How are\b you?")          
+
+# print("Python is a Programming Language\r1234567")
+
+"""
+Hello\namste\thank you\bye
+"""
+
+# print("Hello\ \bnamaste\ \bthank you\ \bbye")
+
+# print("Hello\\namaste\\thank you\\bye")
+
+# print("Tanush Said:\"Good Evening\"")
+
+"""
+\t is used to give tab, but \\\t used to print \t.
+"""
+
+# print("\\t is used to give tab, \\\\t is used to print \\t")
+
+# print("\\\\\\\\t")
+
+"""
+Alpha Said Beta:"Gamma is teasing Theta, but gamma said 'I didn't tease Theta, I tease Pi'"
+"""
+
+print("Alpha Said Beta:\"Gamma is teasing Theta, but gamma said 'I didn't tease Theta, I tease Pi'\"")
+
+
+print('Alpha Said Beta:"Gamma is teasing Theta, but gamma said \'I didn\'t tease Theta, I tease Pi\'"')
+
+
+
+
+
---
+++ first.py
@@ -0,0 +1,24 @@
+# Single Line Comment
+
+"""
+Multi
+Line
+Comments
+"""
+
+'''
+Multi
+Line
+Comments
+'''
+
+print("Hello World")
+print('Hello World')
+
+
+"""
+1) Tanush Said : "Hello! Good Evening"
+2) Prachi said: 'HI! How are you?'
+3) Hello\namaste\thank you\bye
+"""
+print("Hello\namaste\thank you\bye")
