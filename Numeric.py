e7d34ae6fcfebc36937c719fe671fea9b2b99e9a
AnjaliGajjar224 <98864149+AnjaliGajjar224@users.noreply.github.com>
2022-08-15 20:37:23 +0530

    Add files via upload

---
+++ Numeric.py
@@ -0,0 +1,73 @@
+"""
+Numeric Data Types:
+1. Integer
+2. Float
+3. Complex
+
+Python is an implicit bind language.
+"""
+
+# a = 125
+# print("Data Type of Variable a is: ",type(a))
+# print("a: ",a)
+
+# b = 123454984513218545613521321216518451321654848974894653213213218946854561321
+# print(type(b))
+# print(b)
+
+# c = 25.58
+# print("Data type of variable c is: ",type(c))
+# print(c)
+
+# d = 1258.87521231894515874841454595
+# print(type(d))
+# print(d)
+
+# e = 3 + 2j       # Real part - 3 , Imaginary part - 2j
+# print(type(e))
+# print(e)
+
+"""
+Take two numbers and find Addition, Subtraction, Multiplication, Division of it.
+"""
+
+"""
+User value:
+------------
+
+input() ---> by default value is string
+"""
+
+# num1 = int(input("Enter number 1: "))
+# print(type(num1))
+# num2 = int(input("Enter number 2: "))
+
+# add = num1 + num2
+
+# print("Addition : ",add)
+
+# # name = input("Enter name: ")
+
+# print("Enter float number : ")
+# a = float(input())
+
+"""
+Take 5 float numbers from the user and find average of the numbers.
+"""
+
+"""
+Arithmetic Operators:
+
++ , - , * , / , %(Remainder), //(floor division), ** (Exponent)
+"""
+
+print(5/2)    # 2.5
+print(5//2)   # 2 
+print(5**2)   # 5 ^ 2 = 25
+
+"""
+A function named pow(base,power)
+"""
+
+print(pow(15,2))         # base = 15 , power = 2
+
